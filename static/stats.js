439ae1ed0c1639d6d56ece4db8233785835a198e
Brandon Chen <bchen00@stuy.edu>
2020-04-29 22:09:26 -0400

    able to read a valid country input into javascript

--- static/stats.js
+++ static/stats.js
@@ -1,1 +1,19 @@
-console.log("hello")
+console.log("hi")
+
+var chosenCountry = document.getElementById("country")
+chosenCountry.addEventListener('input',function(e){check(this)})
+
+var list = document.getElementById("allCountries")
+
+
+var check = function(e){//checks if input is a valid country
+  for (var i = 0; i < list.childElementCount; i++){
+    if (list.children[i].value.localeCompare(e.value) == 0){
+      createTimeGraph(e.value)//e.value is the chosen country
+    }
+  }
+}
+
+var createTimeGraph = function(e){
+  console.log(e)
+}
