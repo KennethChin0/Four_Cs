98f17e7c9c950002856d3dc484892c7c96dedcb4
Brandon Chen <bchen00@stuy.edu>
2020-04-29 22:30:05 -0400

    filtered data for specified country

--- static/stats.js
+++ static/stats.js
@@ -15,5 +15,14 @@ var check = function(e){//checks if input is a valid country
 }
 
 var createTimeGraph = function(e){
-  console.log(e)
+  var filteredData = []//new data array with only the specified country data
+  //needs a separate checker for United States because US is part of different csv
+  d3.csv("static/data/countries-aggregated.csv").then(function(data){
+    for (var i = 0; i < data.length; i++){
+      if (data[i].Country.localeCompare(e) == 0){
+        filteredData.push(data[i])
+      }
+    }
+  })
+  //console.log(filteredData)
 }
